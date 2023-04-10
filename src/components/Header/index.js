9bcc1d4557e395b2cc43df52a565c685d17c7a79
gopi309 <gopile309@gmail.com>
2023-04-10 02:32:07 +0000

    message

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const Header = () => (
+  <ul className="header-container">
+    <Link to="/">
+      <li className="list-items"> Home </li>
+    </Link>
+    <Link to="/about">
+      <li className="list-items"> About </li>
+    </Link>
+  </ul>
+)
+
+export default Header
